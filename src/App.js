62b79358f8b9c21d8dc142ab2cd41fdb9e36d938
chidai435 <daivcpc06544@fpt.edu.vn>
2024-09-16 17:08:19 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// src/App.js
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdminLayout from './layouts/AdminLayout';
+import ClientLayout from './layouts/ClientLayout';
+import AdminDashboard from './pages/AdminDashboard';  // Đảm bảo đường dẫn chính xác
+import ClientDashboard from './pages/ClientDashboard';  // Đảm bảo đường dẫn chính xác
+import RoomManagement from './components/Admin/RoomManagement';
+import CustomerManagement from './components/Admin/CustomerManagement';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Admin routes */}
+        <Route path="/admin/*" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="rooms" element={<RoomManagement />} />
+          <Route path="customers" element={<CustomerManagement />} />
+        </Route>
+
+        {/* Client routes */}
+        <Route path="/client/*" element={<ClientLayout />}>
+          <Route path="dashboard" element={<ClientDashboard />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
