384e63fdc8ca53ccc2b8d1973411f0e46a5957c0
chidai435 <daivcpc06544@fpt.edu.vn>
2024-09-16 17:08:28 +0700

    Create ClientLayout.js

---
+++ src/layouts/ClientLayout.js
@@ -0,0 +1,13 @@
+// src/pages/ClientDashboard.js
+import React from 'react';
+
+const ClientDashboard = () => {
+  return (
+    <div>
+      <h2>Client Dashboard</h2>
+      <p>Chào mừng bạn đến với hệ thống đặt phòng.</p>
+    </div>
+  );
+};
+
+export default ClientDashboard;
