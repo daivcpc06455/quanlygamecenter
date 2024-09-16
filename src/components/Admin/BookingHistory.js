d0ac0e74485171a78e8c8d69eeed7f8a892026ff
chidai435 <daivcpc06544@fpt.edu.vn>
2024-09-16 17:08:23 +0700

    Create BookingHistory.js

---
+++ src/components/Admin/BookingHistory.js
@@ -0,0 +1,17 @@
+// src/components/Admin/BookingHistory.js
+import React from 'react';
+
+const BookingHistory = () => {
+  return (
+    <div>
+      <h2>Lịch sử đặt phòng</h2>
+      <ul>
+        <li>Khách hàng 1 - Phòng 1 - Ngày: 01/01/2024</li>
+        <li>Khách hàng 2 - Phòng 2 - Ngày: 02/01/2024</li>
+        {/* Thêm các lịch sử đặt phòng khác */}
+      </ul>
+    </div>
+  );
+};
+
+export default BookingHistory;
