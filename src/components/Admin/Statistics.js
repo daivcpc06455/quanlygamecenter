dc422ce4447707d10e35e0526f90956f83dbfe2e
chidai435 <daivcpc06544@fpt.edu.vn>
2024-09-16 17:09:21 +0700

    Create Statistics.js

---
+++ src/components/Admin/Statistics.js
@@ -0,0 +1,15 @@
+// src/components/Admin/Statistics.js
+import React from 'react';
+
+const Statistics = () => {
+  return (
+    <div>
+      <h2>Thống kê</h2>
+      <p>Tổng số phòng đặt: 100</p>
+      <p>Tổng doanh thu: 200 triệu VND</p>
+      {/* Thêm các thống kê khác */}
+    </div>
+  );
+};
+
+export default Statistics;
