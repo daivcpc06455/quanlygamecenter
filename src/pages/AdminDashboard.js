56e18633f39116e5da5ae129b0706e94eb58b57c
chidai435 <daivcpc06544@fpt.edu.vn>
2024-09-16 17:08:31 +0700

    Create AdminDashboard.js

---
+++ src/pages/AdminDashboard.js
@@ -0,0 +1,13 @@
+// src/pages/AdminDashboard.js
+import React from 'react';
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h2>Admin Dashboard</h2>
+      <p>Quản lý hệ thống phòng net từ đây.</p>
+    </div>
+  );
+};
+
+export default AdminDashboard;
