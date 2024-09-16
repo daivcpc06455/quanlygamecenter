84132fc89586f925fa14b5723df32a76057d673b
chidai435 <daivcpc06544@fpt.edu.vn>
2024-09-16 17:08:56 +0700

    Create RoomList.js

---
+++ src/components/Client/RoomList.js
@@ -0,0 +1,17 @@
+// src/components/Client/RoomList.js
+import React from 'react';
+
+const RoomList = () => {
+  return (
+    <div>
+      <h2>Danh sách phòng</h2>
+      <ul>
+        <li>Phòng 1 - VIP</li>
+        <li>Phòng 2 - Standard</li>
+        {/* Thêm các phòng khác */}
+      </ul>
+    </div>
+  );
+};
+
+export default RoomList;
