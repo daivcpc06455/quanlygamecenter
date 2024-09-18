0aea8a4e56c78fa8767c7378be0e5f9449b32c7d
kickboy1324 <kickboy1324@gmail.com>
2024-09-18 19:28:25 +0700

    Update RoomList.js

--- src/components/Client/RoomList.js
+++ src/components/Client/RoomList.js
@@ -1,15 +1,21 @@
-// src/components/Client/RoomList.js
 import React from 'react';
+import ClientDashboard from '../../pages/ClientDashboard';
+import RoomBooking from '../../components/Client/RoomBooking';
+import './Css/RoomList.css';
 
 const RoomList = () => {
+  // Danh sách các phòng
+  const rooms = [
+    { id: 1, name: 'Phòng VIP 1', available: 5, busy: 2, broken: 1, config: 'i7, 16GB RAM, RTX 3070' },
+    { id: 2, name: 'Phòng Thường 1', available: 3, busy: 5, broken: 0, config: 'i5, 8GB RAM, GTX 1060' },
+    { id: 3, name: 'Phòng Đôi', available: 4, busy: 3, broken: 2, config: 'Ryzen 5, 16GB RAM, RTX 3060' },
+  ];
+
   return (
     <div>
-      <h2>Danh sách phòng</h2>
-      <ul>
-        <li>Phòng 1 - VIP</li>
-        <li>Phòng 2 - Standard</li>
-        {/* Thêm các phòng khác */}
-      </ul>
+      {/* Truyền dữ liệu rooms vào cả ClientDashboard và RoomBooking */}
+      <ClientDashboard rooms={rooms} />
+      <RoomBooking rooms={rooms} />
     </div>
   );
 };
