9a6b7e7c0f536fda66b65585cc21f810140aa647
VuluanPC06483 <luanhvPC06483@fpt.edu.vn>
2024-09-18 16:49:12 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // src/index.js
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Đảm bảo có cả JS và Popper.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
