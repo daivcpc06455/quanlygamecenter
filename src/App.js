6c9089ddab5a2235c58f1f87a4f22813b0fada95
HuaTan2709 <tanminhhua123@gmail.com>
2024-09-18 16:53:01 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-// index.js hoặc App.js
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminLayout from './layouts/AdminLayout';
 import ClientLayout from './layouts/ClientLayout';
@@ -9,30 +8,33 @@ import AdminDashboard from './pages/AdminDashboard';
 import ClientDashboard from './pages/ClientDashboard';
 import RoomManagement from './components/Admin/RoomManagement';
 import CustomerManagement from './components/Admin/CustomerManagement';
-import BookingHistory from './components/Admin/BookingHistory'; // Thêm quản lý đơn đặt phòng
-
-
-
-// import UserManagement from './components/Admin/UserManagement';
+import BookingHistory from './components/Admin/BookingHistory';
+import Cart from './components/Client/Cart'; // Import Cart
+import { CartProvider } from './contexts/CartContext'; // Import CartProvider
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Admin routes */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="dashboard" element={<AdminDashboard />} />
-          <Route path="rooms" element={<RoomManagement />} />
-          <Route path="customers" element={<CustomerManagement />} />
-          <Route path="bookings" element={<BookingHistory />} /> {/* Quản lý đơn đặt phòng */}
-        </Route>
+    <CartProvider>
+      <Router>
+        <Routes>
+          {/* Admin routes */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="rooms" element={<RoomManagement />} />
+            <Route path="customers" element={<CustomerManagement />} />
+            <Route path="bookings" element={<BookingHistory />} />
+          </Route>
 
-        {/* Client routes */}
-        <Route path="/client" element={<ClientLayout />}>
-          <Route path="dashboard" element={<ClientDashboard />} />
-        </Route>
-      </Routes>
-    </Router>
+          {/* Client routes */}
+          <Route path="/client" element={<ClientLayout />}>
+            <Route path="dashboard" element={<ClientDashboard />} />
+          </Route>
+
+          {/* Cart route */}
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
