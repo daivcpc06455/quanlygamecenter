280bdb797e1d6a34099c13bd6730a1cd53f56fc7
kickboy1324 <kickboy1324@gmail.com>
2024-09-18 19:28:10 +0700

    Create Footerclient.js

---
+++ src/components/Client/Footerclient.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Css/FooterClient.css';
+
+const FooterClient = () => {
+  return (
+    <footer className="footer-client">
+      <p>Five Star Gaming &copy; 2024. Tất cả các quyền được bảo lưu.</p>
+      <p>Liên hệ: 0123 456 789 | Email: support@fivestargaming.com</p>
+    </footer>
+  );
+};
+
+export default FooterClient;
