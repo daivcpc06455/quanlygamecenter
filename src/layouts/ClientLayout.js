9e6782c55ca2151c68c84cbd3dac4f237dd8ace4
chidai435 <daivcpc06544@fpt.edu.vn>
2024-09-18 12:52:27 +0700

    Update ClientLayout.js

--- src/layouts/ClientLayout.js
+++ src/layouts/ClientLayout.js
@@ -1,11 +1,37 @@
-// src/pages/ClientDashboard.js
-import React from 'react';
+import React, { useState } from 'react';
+import RoomBooking from '../components/Client/RoomBooking';
+import FoodMenu from '../components/Client/FoodMenu';
+import Cart from '../components/Client/cart';
+import Checkout from '../components/Client/Checkout';
 
 const ClientDashboard = () => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+
+  const addRoomToCart = (room) => {
+    setCart([...cart, room]);
+  };
+
+  const removeFromCart = (index) => {
+    const newCart = cart.filter((_, i) => i !== index);
+    setCart(newCart);
+  };
+
+  const handleCheckout = () => {
+    alert('Thanh toán thành công!');
+    setCart([]); // Xóa giỏ hàng sau khi thanh toán
+  };
+
   return (
     <div>
-      <h2>Client Dashboard</h2>
-      <p>Chào mừng bạn đến với hệ thống đặt phòng.</p>
+      <h1>Giao Diện Người Dùng</h1>
+      <RoomBooking addRoomToCart={addRoomToCart} />
+      <FoodMenu addToCart={addToCart} />
+      <Cart cart={cart} removeFromCart={removeFromCart} />
+      <Checkout cart={cart} handleCheckout={handleCheckout} />
     </div>
   );
 };
